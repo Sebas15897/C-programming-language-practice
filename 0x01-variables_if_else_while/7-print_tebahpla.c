2641394b5d77793a692bc09b3c18b3ce1fae983d
Sebas15897 <sebastiancontreras15897@gmail.com>
2020-09-18 03:57:48 +0000

    firstcommit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Putchar Z to A
+ * Return: 0 for success
+ */
+int main(void)
+{
+char ch = 'a';
+
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+
+putchar('\n');
+
+return (0);
+}
