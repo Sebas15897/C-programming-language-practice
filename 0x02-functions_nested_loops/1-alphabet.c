51597b45a4db8b89d94acc036ae5bd9325ab3330
Sebas15897 <sebastiancontreras15897@gmail.com>
2020-09-23 00:49:41 +0000

    firstcommit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,22 @@
+  
 #include "holberton.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet_x10 - This function print alphabet x10
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-void print_alphabet(void)
-{
-  int i;
 
-  for (i = 97; i <= 122; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
+void print_alphabet_x10(void)
+{
+int a, b;
+
+for (a = 0; a <= 9 ; a++)
+{
+for (b = 97; b <= 122; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
 }
