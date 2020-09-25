06f56ec96bd22bcea4d7f62b4c90e80ead64e97c
Sebas15897 <sebastiancontreras15897@gmail.com>
2020-09-25 04:44:59 +0000

    firstcommit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,16 @@
  */
 void more_numbers(void)
 {
-  int i, c;
+int i, c;
 
-  for (i = 0; i < 10; i++)
-    {
-      for (c = 0; c < 15; c++)
-	{
-	  if (c >= 10)
-	    _putchar((c / 10) + 48);
-	  _putchar((c % 10) + 48);
-	}
-      _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+for (c = 0; c < 15; c++)
+{
+if (c >= 10)
+_putchar((c / 10) + 48);
+_putchar((c % 10) + 48);
+}
+_putchar('\n');
+}
 }
