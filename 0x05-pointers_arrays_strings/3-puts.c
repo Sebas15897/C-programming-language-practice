58c07cdf90c620b80413a1e34c9d0477c56b5b07
Sebas15897 <sebastiancontreras15897@gmail.com>
2020-09-30 03:29:23 +0000

    firstcommit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _puts - put a string
+ * @str: array of chars
  */
-int main(void)
-{
-char *str;
 
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-_puts(str);
-return (0);
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
