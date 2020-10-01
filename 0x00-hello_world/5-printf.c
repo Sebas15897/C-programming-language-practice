1736300c6a99391bb534835a9831d2f8f7e1bbe8
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2020-10-01 17:48:45 -0500

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,11 +6,5 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-=======
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
->>>>>>> 903de5e76a00e0adf8218b929c786851db4b3957
-}
