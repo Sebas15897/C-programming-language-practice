8a5c47ddd5cd7cc03cd323f6110295a2b6dd3ee7
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2020-10-15 23:15:27 -0500

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,40 +11,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, k, len1, len2;
-char *strc;
+	unsigned int i, j, k, len1, len2;
+	char *strc;
 
-len1 = len2 = 0;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (i = 0; s1[i]; i++)
-{
-len1++;
+	len1 = len2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i]; i++)
+	{
+		len1++;
 
-}
-for (j = 0; s2[j]; j++)
-{
-len2++;
-}
-if (!(n >= len2))
-{
-len2 = n;
-strc = malloc(sizeof(char) * (len1 + len2 + 1));
-if (strc == NULL)
-{
-return (NULL);
-}
-for (k = 0; k < len1; k++)
-{
-strc[k] = s1[k];
-}
-for (k = 0; k < len2; k++)
-{
-strc[k + len1] = s2[k];
-}
-strc[len1 + len2] = '\0';
-}
-return (strc);
+	}
+	for (j = 0; s2[j]; j++)
+	{
+		len2++;
+	}
+	if (!(n >= len2))
+	{
+		len2 = n;
+		strc = malloc(sizeof(char) * (len1 + len2 + 1));
+		if (strc == NULL)
+		{
+			return (NULL);
+		}
+		for (k = 0; k < len1; k++)
+		{
+			strc[k] = s1[k];
+		}
+		for (k = 0; k < len2; k++)
+		{
+			strc[k + len1] = s2[k];
+		}
+		strc[len1 + len2] = '\0';
+	}
+	return (strc);
 }
