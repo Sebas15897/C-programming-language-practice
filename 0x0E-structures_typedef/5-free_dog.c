5c56930928825e47644c13bf8c58e2872e3513cb
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2020-10-19 22:39:55 -0500

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
+ * free_dog - free the space of a dog structure
+ * @d: the dog
  *
- * Return: Always 0.
  */
-int main(void)
-{
-    dog_t *my_dog;
 
-    my_dog = new_dog("Django", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
-    return (0);
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+
 }
