cdd4a29ee1196ae2613342ef60f12ff00e3c182e
Sebas15897 <sebastiancontreras15897@gmail.com>
2020-11-28 02:05:30 +0000

    firstcommit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ *print_list- print the list in a struct
+ *
+ *@h: pointer to head of list
+ *Return:zero
+ */
+
+
+size_t print_list(const list_t *h)
+
+{
+
+size_t size = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+
+h = h->next;
+size++;
+
+}
+
+return (size);
+}
