ebcb2991119ae62eb8ba01b52be50b3dd79b2ef3
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-15 21:15:26 -0500

    fix

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,18 @@
 #include "hash_tables.h"
-
 /**
- * hash_djb2 - a function that implementing the djb2 algorithm
- * @str: a pointer that point to a string
- * Return: unsigned long int hash
+ * hash_djb2 - dj function
+ * @str: growing up string
+ * Return: index position
  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-  unsigned long int hash;
-  int c;
+	unsigned long int hash  = 5381;
+	int c;
 
-  hash = 5381;
-  while ((c = *str++))
-    {
-      hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-  return (hash);
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
