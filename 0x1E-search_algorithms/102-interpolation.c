b03b2704e183d2bd493ca1998020fa88d476be27
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-04-20 13:58:03 -0500

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -9,37 +9,37 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-  size_t low = 0, high = size - 1, pos;
+	size_t low = 0, high = size - 1, pos;
 
-  if (array == NULL)
-    return (-1);
+	if (array == NULL)
+		return (-1);
 
-  pos = low + (((double)(high - low) / (array[high] - array[low])) *
-	       (value - array[low]));
-  if (pos > size)
-    {
-      printf("Value checked array[%lu] is out of range\n", pos);
-      return (-1);
-    }
+	pos = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
+	if (pos > size)
+	{
+		printf("Value checked array[%lu] is out of range\n", pos);
+		return (-1);
+	}
 
-  while ((array[high] != array[low]) && (value >= array[low]) &&
-	 (value <= array[high]))
-    {
-      pos = low + (((double)(high - low) / (array[high] - array[low])) *
-		   (value - array[low]));
+	while ((array[high] != array[low]) && (value >= array[low]) &&
+	       (value <= array[high]))
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+					(value - array[low]));
 
-      printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-      if (array[pos] < value)
-	low = pos + 1;
-      else if (value < array[pos])
-	high = pos - 1;
-      else
-	return (pos);
-    }
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (value < array[pos])
+			high = pos - 1;
+		else
+			return (pos);
+	}
 
-  if (value == array[low])
-    return (low);
-  else
-    return (-1);
+	if (value == array[low])
+		return (low);
+	else
+		return (-1);
 }
