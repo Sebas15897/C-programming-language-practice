f213246dca85a52f152dcf1ec275a26f9d274988
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-04-20 13:35:23 -0500

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,40 +1,63 @@
 #include "search_algos.h"
-
 /**
- * binary_search - function that searches for a value in an array
- * of integers using the Binary search algorithm
- * @array: input array elements
- * @size: the size of array
- * @value: is the value that algorithm to search
- * Return: Always EXIT_SUCCESS
+ * binary_search - search for a value in a array with o(logn)
+ * @array: array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index of the value
  */
 int binary_search(int *array, size_t size, int value)
 {
-  int i;
-  int j;
-  int left = 0;
-  int right = size - 1;
+	if (!array)
+		return (-1);
+	return (recursive_search(0, size - 1, value, array));
+}
 
-  if (array == NULL)
-    return (-1);
 
-  while (left <= right)
-    {
-      printf("Searching in array: ");
-      for (i = left; i <= right; i++)
+/**
+ * recursive_search - search by half
+ * @start: starting point
+ * @end: end point
+ * @value: searched_value
+ * @array: array array
+ * Return: Nothing
+ */
+int recursive_search(int start, int end, int value, int *array)
+{
+	int middle = start + (end - start) / 2;
+
+	_print(start, end, array);
+	if (start == end && value != array[middle])
+		return (-1);
+	else if (value > array[middle])
 	{
-	  if (i == right)
-	    printf("%i\n", array[i]);
-	  else
-	    printf("%i, ", array[i]);
+		middle = recursive_search(middle + 1, end, value, array);
 	}
-      j = left + (right - left) / 2;
-      if (array[j] == value)
-	return (j);
-      else if (value > array[j])
-	left = j + 1;
-      else if (value < array[j])
-	right = j - 1;
-    }
-  return (-1);
+	else if (value < array[middle])
+	{
+		middle = recursive_search(start, middle - 1, value, array);
+	}
+	return (middle);
+}
+
+
+/**
+ * _print - prints every try to find the value
+ * @start: starting point
+ * @end: end point
+ * @array: array array
+ * Return: Nothing
+ */
+void _print(int start, int end, int *array)
+{
+	int i = 0;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		if (i != end)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
 }
