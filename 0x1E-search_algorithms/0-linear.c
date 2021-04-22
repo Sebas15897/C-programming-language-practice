4b9b42acd8f4f910f001b95072018d2c986c12cf
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-04-22 13:17:12 -0500

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,41 +1,26 @@
 #include "search_algos.h"
 
 /**
- * binary_search - function that searches for a value in a sorted array of
- * integers using the Binary search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value:  the value to search for
- * Return: index where value is located
- * If value is not present in array or if array is NULL, return -1
- */
+* linear_search - function that searches for a value in an array of integers
+* @array: Pointer to first element of the array
+* @size: Number of elements in the array
+* @value: Value to search
+* Return: The index found or -1 if failed
+*/
 
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
-	unsigned int lowerLimit = 0;
-	unsigned int upperLimit = size - 1;
-	unsigned int middle, i;
+
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
-	while (lowerLimit <= upperLimit)
+
+	for (i = 0; i <= size - 1; i++)
 	{
-		printf("Searching in array: ");
-		for (i = lowerLimit; i <= upperLimit; i++)
-		{
-			printf("%d", array[i]);
-			if (i < upperLimit)
-				printf(", ");
-			else
-				printf("\n");
-		}
-		middle = (lowerLimit + upperLimit) / 2;
-		if (array[middle] == value)
-			return (middle);
-		if (array[middle] < value)
-			lowerLimit = middle + 1;
-		if (array[middle] > value)
-			upperLimit = middle - 1;
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
 	}
 	return (-1);
 }
